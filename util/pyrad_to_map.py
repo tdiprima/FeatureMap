18bb11e2cdb4ec55cea2873a05ffc882e497d906
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-05-21 10:31:49 -0400

    update

--- util/pyrad_to_map.py
+++ util/pyrad_to_map.py
@@ -33,10 +33,10 @@ def create_csv(input, output, red, r_name, green, g_name):
 
     x = 'patch_x'
     y = 'patch_y'
-    
+
     # Columns
     modified = df[[x, y, red, green]]
-    
+
     # Sort
     modified = modified.sort_values([x, y], ascending=[1, 1])
 
@@ -62,26 +62,81 @@ def create_csv(input, output, red, r_name, green, g_name):
 
     # Columns
     modified = modified[['i', 'j', 'r', 'g', blue]]
-    
+
     # Nice name
     modified = modified.rename(index=str, columns={"r": r_name})
     modified = modified.rename(index=str, columns={"g": g_name})
-    print(modified)
 
     # Write
     with open(output, 'a') as f:
         modified.to_csv(f, mode='a', header=False, index=False)
 
 
-# Do for all files in directory:
-# cwd = os.getcwd()
-# for filename in os.listdir(cwd):
-#     if filename.endswith(".csv"):
-#         create_csv(os.path.join(cwd, filename), filename + '.1')
+def do_one_feature(input, output):
+    df = pd.read_csv(input)
 
-# Process one file:
-red = 'nuclei_ratio'
-r_name = 'Nuclear Ratio'
-green = 'fg_glcm_Correlation'
-g_name = 'Fg Glcm Correlation'
-create_csv('input.csv', red + '_' + green + '.csv', red, r_name, green, g_name)
+    imw = df['image_width'].iloc[0]
+    imh = df['image_height'].iloc[0]
+    pw = df['patch_width'].iloc[0]
+    ph = df['patch_height'].iloc[0]
+
+    obj = {"img_width": str(imw),
+           "img_height": str(imh),
+           "patch_w": str(pw),
+           "patch_h": str(ph),
+           "png_w": str(np.ceil(imw / pw).astype(int)),
+           "png_h": str(np.ceil(imh / ph).astype(int))}
+
+    # print(obj)
+
+    with open(output, 'w') as f:
+        f.write(json.dumps(obj) + '\n')
+        f.write('i,j,Nuclear Ratio,Cancer,Tissue\n')
+
+    cols = list(df.columns)
+    modified = df[cols[5:7] + cols[11:12]]
+    modified = modified.sort_values(['patch_x', 'patch_y'], ascending=[1, 1])
+    modified['i'] = modified['patch_x'] / df['patch_width']
+    modified['j'] = modified['patch_y'] / df['patch_height']
+    modified['n'] = modified['nuclei_ratio'] * 255
+
+    modified.i = np.ceil(modified.i).astype(int)
+    modified.j = np.ceil(modified.j).astype(int)
+    modified.n = np.ceil(modified.n).astype(int)
+
+    modified.drop("nuclei_ratio", axis=1, inplace=True)
+    modified = modified.rename(index=str, columns={"n": "Nuclear Ratio"})
+
+    cols = list(modified.columns)
+    modified = modified[cols[2:]]
+    modified['Cancer'] = 0
+    modified['Tissue'] = 0
+    modified.loc[modified['Nuclear Ratio'] > 0, ['Tissue']] = ['255']
+
+    # print(modified)
+
+    # modified.to_csv(output, index=False)
+    with open(output, 'a') as f:
+        modified.to_csv(f, mode='a', header=False, index=False)
+
+
+def process_dir():
+    # Do for all files in directory:
+    cwd = os.getcwd()
+    for filename in os.listdir(cwd):
+        if filename.endswith(".csv"):
+            red = 'nuclei_ratio'
+            r_name = 'Nuclear Ratio'
+            green = 'fg_glcm_Correlation'
+            g_name = 'Fg Glcm Correlation'
+            create_csv(os.path.join(cwd, filename), os.path.join(cwd, filename + '.1'), red, r_name, green, g_name)
+            # do_one_feature(os.path.join(cwd, filename), filename + '.1')
+
+
+def process_one():
+    # Process one file:
+    red = 'nuclei_ratio'
+    r_name = 'Nuclear Ratio'
+    green = 'fg_glcm_Correlation'
+    g_name = 'Fg Glcm Correlation'
+    create_csv('input.csv', red + '_' + green + '.csv', red, r_name, green, g_name)
