1bd320c5f784c87757a9dfb40ddaa01080a41b3d
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-07-12 13:39:58 -0400

    wip

--- tilmap.js
+++ tilmap.js
@@ -15,12 +15,12 @@ tilmap = function () {
     tilmap.slide = getQueryVariable('slideId', str); // drupal node of slide
     tilmap.mode = getQueryVariable('mode', str); // camic toggle switch
 
-    promiseA = pathdb_util.fetch_data(tilmap.map);
+    promiseA = fetch_data(tilmap.map);
     promiseA.then(function (result) {
       if (result === null) {
         console.log('Abort.')
       } else {
-        console.log('result');
+        console.log('new');
         console.log(result);
         // tilmap.dataUri = result;
         // // download(tilmap.slide + '.png', result);
@@ -32,6 +32,46 @@ tilmap = function () {
     alert('Expecting parameters: mode, slideId, and map');
   }
 
+};
+
+fetch_data = function (url) {
+
+  return fetch(url).then(function (response) {
+    console.log('url', url);
+    console.log('response', response);
+
+    if (response.ok) {
+
+      let headers = response.headers.get('content-type');
+      let data;
+
+      if (headers.toLowerCase().indexOf("application/json") === -1) {
+        data = response.text();
+      } else {
+        data = response.json();
+      }
+      return data; // fulfillment value given to user
+    } else {
+      // Alert that there's an error
+      setTimeout((function () {
+        if (url !== null) {
+          let x = url.split('/');
+          let y = x.length;
+          alert(x[y - 1] + ' ' + response.status + ' ' + response.statusText);
+          return false;
+
+        } else {
+          alert(response.status + ' ' + response.statusText);
+          return false;
+
+        }
+      }), 1000);
+
+    }
+    throw new Error(response.status + ' ' + response.statusText);
+  }).catch(function (error) {
+    console.log('There has been a problem with your fetch operation: ', error.message);
+  });
 };
 
 function scaler(canvas) {
