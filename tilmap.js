b863d2c8a0ee738a0595043cbf2ac0769a0a57de
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-09-03 15:48:35 -0400

    Update tilmap.js

--- tilmap.js
+++ tilmap.js
@@ -836,6 +836,8 @@ tilmap.segment = function (event, doTranspire = true) {
       //countTil+=(d[0]*tr>=sv)
       //return (Math.max(d[1]*cr,d[0]*tr))>=sv
       //return ((Math.max(d[1]*cr,d[0]*tr))>=sv)&(d[2]==255) //leave non-tissue out
+      //countCancer+=(d[1]*cr>=sv)&(d[2]==255) //tissuing out cancer and til as well
+      //countTil+=(d[0]*tr>=sv)&(d[2]==255) //tissuing out cancer and til as well
       countGreen += (d[1] * cr >= sv) & (d[2] > 0); // use sv for count
       countRed += (d[0] * tr >= sv) & (d[2] > 0);
       return ((Math.max(d[1] * cr, d[0] * tr)) >= sv1) & (d[2] > 0); // use normal sv for mask
